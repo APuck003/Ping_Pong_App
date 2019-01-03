d47f951e3f84cb524651117e4f94f9ca7c485e99
rdaley321 <rdaley321@yahoo.com>
2019-01-03 17:54:21 -0600

    uniqueness validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_many :comments
 
   validates :username, presence: true
+  validates :username, uniqueness: true
   validates :password, confirmation: true
   validates :password_confirmation, presence: true
 end
